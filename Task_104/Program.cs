78521b7175eb32ebab696791e6cfb5bf03529540
Fokiruna <alekseyyarchuk@gmail.com>
2017-05-17 19:52:22 +0300

    Добавленые новые решния

---
+++ Task_104/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_104
+{
+    class Program
+    {
+        static public List<int[]> listMassive;
+        static public List<double> answers = new List<double>();
+        static void Main(string[] args)
+        {
+            
+            ReadFile();
+            Gerone();
+            Print();
+
+
+        }
+        static void Print()
+        {
+            
+            foreach (var item in answers)
+            {
+                Console.Write(item.ToString().Replace(",", ".") + " ");
+            }
+            Console.ReadKey();
+        }
+        static void Gerone()
+        {
+            for (int i = 0; i < listMassive.Count; i++)
+            {
+                //расстояние между точками 
+                //(x2 - x1)**2 + (y2-y1)**2
+                double a = Math.Sqrt(Math.Pow((listMassive[i][2] - listMassive[i][0]),2) + Math.Pow((listMassive[i][3] - listMassive[i][1]),2));
+                //(x3 - x1)**2 + (y3-y1)**2
+                double b = Math.Sqrt(Math.Pow((listMassive[i][4] - listMassive[i][0]),2) + Math.Pow((listMassive[i][5] - listMassive[i][1]),2));
+                //(x3 - x2)**2 + (y3-y2)**2
+                double c = Math.Sqrt(Math.Pow((listMassive[i][4] - listMassive[i][2]),2)  + Math.Pow((listMassive[i][5] - listMassive[i][3]),2));
+                //полупериметр
+                double p = (a + b + c) / 2;
+                //формула герона
+                double gerone = Math.Sqrt(p * ((p - a) * (p - b) * (p - c)));
+                answers.Add(Math.Round(gerone,1));
+            }
+            
+        }
+        static void ReadFile()
+        {
+            listMassive = new List<int[]>();
+           using (FileStream filestr = new FileStream(@"C:\Work\Codeabbey\Task_104\file.txt", FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader strRead = new StreamReader(filestr, Encoding.UTF8))
+                {
+                    while (!strRead.EndOfStream)
+                    {
+                        listMassive.Add(strRead.ReadLine().Split(' ').Select(Int32.Parse).ToArray());
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Task_55/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_55
+{
+    //Взят алгоримт Кнута-Мориса-Прата
+    class Program
+    {
+        static public List<string> list;
+        static public List<string> repeatlist;
+        static void Main(string[] args)
+        {
+            ReadFile();
+            RepeatedString();
+            BubleSort();
+            Print();
+            //string tar = "san and linux training";
+
+            //string pat = "sa";
+
+            //if (KMP(pat, tar))
+            //    Console.WriteLine("есть");
+
+            //else
+            //    Console.WriteLine("нету");
+
+            //string s1 = "bb";
+            //string s2 = "aa";
+            //int r = s1.CompareTo(s2);
+            //if (r < 0)
+            //{
+            //    Console.WriteLine("Совпадение строк");
+            //}
+            //else Console.WriteLine("Несовпадение строк");
+
+        }
+
+        static void Print()
+        {
+            foreach (var item in repeatlist)
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadKey();
+        }
+
+        static void BubleSort()
+        {
+            repeatlist = repeatlist.Distinct().ToList();
+            bool flag = false;
+
+            while (!flag)
+            {
+                flag = true;
+                for (int i = 0; i < repeatlist.Count-1; i++)
+                {
+                    int r = repeatlist[i].CompareTo(repeatlist[i + 1]);
+                    if (r > 0)
+                    {
+                        flag = false;
+                        Swap(i, i + 1);
+                    }
+                       
+                }
+            }
+        }
+
+        static void RepeatedString()
+        {
+            repeatlist = new List<string>();
+            List<string> tmp = new List<string>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                tmp.Add(list[i]);
+            }
+            int iter = 0;
+            foreach (var item in list)
+            {
+                tmp.RemoveAt(iter);
+                string str = string.Join(" ", tmp.ToArray());
+                if (KMP(item, str))
+                    repeatlist.Add(item);
+                
+
+            }
+          
+        }
+
+        static void preKMP(string pattern, int[] f)
+        {
+            int m = pattern.Length, k;
+            f[0] = -1;
+            for (int i = 1; i < m; i++)
+            {
+                k = f[i - 1];
+                while (k >= 0)
+                {
+                    if (pattern[k] == pattern[i - 1])
+
+                        break;
+                    else
+                      k = f[k];
+                }
+                f[i] = k + 1;
+            }
+
+        }
+
+
+
+        //check whether target string contains pattern 
+
+        static bool KMP(string pattern, string target)
+        {
+            int m = pattern.Length;
+            int n = target.Length;
+            int[] f = new  int[m];
+            preKMP(pattern, f);
+            int i = 0;
+            int k = 0;
+            while (i < n)
+            {
+                if (k == -1)
+                {
+                    i++;
+                    k = 0;
+                }
+                else if (target[i] == pattern[k])
+                {
+                    i++;
+                    k++;
+                    if (k == m)
+                        return true; //false
+                }
+                else
+                    k = f[k];
+
+            }
+            return false; //true
+
+        }
+
+        static void Swap(int i, int j)
+        {
+            var t = repeatlist[i];
+            repeatlist[i] = repeatlist[j];
+            repeatlist[j] = t;
+        }
+        static void ReadFile()
+        {
+            list = File.ReadAllText(@"C:\Work\Codeabbey\Task_55\file.txt", Encoding.UTF8).Split(' ').ToList();
+           
+            //using (FileStream filestr = new FileStream(@"C:\Work\Codeabbey\Task_55\file.txt", FileMode.Open, FileAccess.Read))
+            //{
+            //    using (StreamReader strRead = new StreamReader(filestr, Encoding.UTF8))
+            //    {
+            //        while (!strRead.EndOfStream)
+            //        {
+            //            list = strRead.ReadLine().Split(' ').ToList();
+                        
+            //        }
+            //    }
+            //}
+        }
+    }
+
+}
