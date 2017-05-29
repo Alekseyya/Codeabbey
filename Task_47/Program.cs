eb905d1998d0b5d32a79a960edd6389c8835374e
Fokiruna <alekseyyarchuk@gmail.com>
2017-05-29 16:59:02 +0300

    Шифр Юлия Цезаря

---
+++ Task_47/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_47
+{
+    class Program
+    {
+        //Список всех строк
+        static public List<string> listStr = new List<string>();
+        static public List<StringBuilder> answerList = new List<StringBuilder>();
+        static public int k=6;
+        static void Main(string[] args)
+        {
+            ReadFile();
+            SplitStr();
+            Print();
+            Console.ReadKey();
+        }
+        static void Print()
+        {
+            foreach (var item in answerList)
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadKey();
+        }
+        //функция разбра прездожения
+        static void SplitStr()
+        {
+            
+            foreach (var str in listStr)  //берем каждое предложение
+            {
+                StringBuilder resultString = new StringBuilder();
+                foreach (char charStr in str) //получаем каждый символ
+                {
+                    if (charStr == ' ' | charStr == '.')
+                    {
+                        resultString.Append(charStr);
+                        continue;
+                    }
+                        
+                    int ASCIINuberChar = (int)charStr;
+                    char resChar = charStr;
+                    int kf = 0;
+                    for (int i = 1; i <= k; i++)
+                    {
+                        
+                        if (resChar == 'A')
+                        {
+                            ASCIINuberChar = 90;
+                            kf = i;
+                        }
+                        resChar = (char)(ASCIINuberChar - i + kf);
+
+                    }
+                    resultString.Append(resChar);
+                }
+                
+                answerList.Add(resultString);
+            }
+            
+        }
+        static void ReadFile()
+        {
+            
+            using (FileStream filestr = new FileStream(@"C:\Work\Codeabbey\Task_47\file.txt", FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader strRead = new StreamReader(filestr, Encoding.UTF8))
+                {
+                    while (!strRead.EndOfStream)
+                    {
+                        listStr.Add(strRead.ReadLine());
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Task_47/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Task_47")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task_47")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("2effbbd0-f24f-4dc5-b82b-417ec73879d8")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
