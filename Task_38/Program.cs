8b63b270f18566ceb6bcdc14167c07ec9377ee6a
Fokiruna <alekseyyarchuk@gmail.com>
2017-06-13 04:00:23 +0300

    Квадратное уравнение

---
+++ Task_38/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_38
+{
+    class Program
+    {
+        static public List<Quadratic> list = new List<Quadratic>();
+        static void Main(string[] args)
+        {
+            ReadFile();
+            Process();
+            Console.ReadKey();
+
+        }
+        static void Process()
+        {
+            foreach (var equation in list)
+            {
+                if (equation.DiscriminantBool)
+                {
+                    Console.Write((-equation.B + Math.Sqrt(Math.Pow(equation.B, 2) - 4 * equation.A * equation.C)) / (2 * equation.A));
+                    Console.Write(" ");
+                    Console.Write((-equation.B - Math.Sqrt(Math.Pow(equation.B, 2) - 4 * equation.A * equation.C)) / (2 * equation.A));
+                    Console.Write("; ");
+                }else
+                {
+                    int b = -equation.B;
+                    int sqrt = (int)Math.Sqrt(equation.Discriminant * -1);
+                    int twoA = (2 * equation.A);
+                    int imp = b/twoA;
+                    int dec = sqrt / twoA;
+                    Console.Write($"{imp}+{dec}i ");
+                    Console.Write($"{imp}-{dec}i; ");
+
+                }
+            }
+        }
+        static void ReadFile()
+        {
+            using (FileStream filestr = new FileStream(@"C:\Work\Codeabbey\Task_38\file.txt", FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader strRead = new StreamReader(filestr, Encoding.UTF8))
+                {
+                    while (!strRead.EndOfStream)
+                    {
+                        List<int> temp = strRead.ReadLine().Split(' ').Select(Int32.Parse).ToList();
+                        list.Add(new Quadratic { A = temp[0], B = temp[1], C = temp[2] });
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Task_38/Quadratic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_38
+{
+    public class Quadratic
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+        public bool DiscriminantBool
+        {
+            get
+            {
+                
+                if (Discriminant >= 0)
+                    return true;
+                return false;
+            }
+        }
+        public int Discriminant
+        {
+            get
+            {
+                return (int)Math.Pow(B, 2) - 4 * A * C;
+
+            }
+            
+        }
+    }
+}
